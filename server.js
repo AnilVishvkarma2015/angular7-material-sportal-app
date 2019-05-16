6a8d0ac31546747f498ddfdfaf845aa0867d0974
Anil Vishvkarma <avish.develoepr@gmail.com>
2019-05-17 00:19:29 +0530

    Added relative path of dist

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-  app.use(express.static(__dirname + '/dist/sportal-mean-app-angular7'));
+  app.use(express.static(__dirname + '/dist/student-portal-client-material'));
 
 app.get('/*', function (req, res) {
-    res.sendFile(path.join(__dirname + '/dist/sportal-mean-app-angular7/index.html'));
+    res.sendFile(path.join(__dirname + '/dist/student-portal-client-material/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
